1d9dfc50c77aea1f98c9b05beb59ecd93f96e895
Nicholas <nicholasmarques@cjr.org.br>
2018-04-05 13:03:28 -0300

    feitos cenarios de update e create de movies

--- features/listing_movies.feature
+++ features/listing_movies.feature
@@ -1,3 +1,4 @@
+@movies
 Feature: Manage Movies
   In order to see all movies
   As a normal user
@@ -7,3 +8,26 @@ Feature: Manage Movies
     Given I have movies titled "Interestelar","Gravidade"
     When I go to the list of movies
     Then I should see "Interestelar","Gravidade" movies
+
+  Scenario: Seeing specific movie
+    Given I have a movie titled "Interestelar"
+    When I go to the list of movies
+    And Clico no link "Interestelar"
+    Then I should see the title and description of Interestelar
+
+  Scenario: Creating a movie
+    Given I am in the home page
+    When I click on link "New Movie"
+    And Put the title "Interestelar", description "It's all relative" and rating 5
+    And click on "Create Movie"
+    Then I should see a message saying "Movie was successfully created!"
+    And current path should be the path to the movies list with the new movie "Interestelar" added
+
+  Scenario: Editing a movie
+    Given I have a movie titled "Interestelar", with description "Description" and rating 4
+    When I go to the list of movies
+    And click the link "Edit Movie"
+    And I put the title "Edited Interestelar", description "Edited Description" and rating 5
+    And click on "Update Movie"
+    Then I should see a message saying "Movie was successfully updated."
+    And current path should be the path to the movies list with the movie edited
--- features/step_definitions/listing_movies_steps.rb
+++ features/step_definitions/listing_movies_steps.rb
@@ -1,3 +1,5 @@
+#Scenario 1
+
 Given(/^I have movies titled "([^"]*)","([^"]*)"$/) do |movie1, movie2|
   @movie1 = Movie.create(title: movie1)
   @movie2 = Movie.create(title: movie2)
@@ -12,4 +14,75 @@ Then(/^I should see "([^"]*)","([^"]*)" movies$/) do |title1, title2|
   @movie2 = Movie.find_by_title(title2)
   expect(page).to have_content(@movie1.title)
   expect(page).to have_content(@movie2.title)
+end
+
+# Scenario 2
+
+Given(/^I have a movie titled "([^"]*)"$/) do |movie|
+  @movie = Movie.create(title: movie, description: "O filme mais massa da história")
+end
+
+When(/^Clico no link "([^"]*)"$/) do |link|
+
+  visit "/"
+  click_link @movie.title
+end
+
+Then(/^I should see the title and description of Interestelar$/) do
+  expect(page).to have_content(@movie.title)
+  expect(page).to have_content(@movie.description)
+end
+
+#Scenario 3
+
+Given(/^I am in the home page$/) do
+  visit "/"
+end
+
+When(/^I click on link "([^"]*)"$/) do |link|
+  click_link link
+end
+
+When(/^Put the title "([^"]*)", description "([^"]*)" and rating (\d+)$/) do |title, description, rating|
+  fill_in "Title", with: title
+  fill_in "Description", with: description
+  fill_in "Rating", with: rating
+end
+
+When(/^click on "([^"]*)"$/) do |button|
+  click_button button
+end
+
+Then(/^I should see a message saying "([^"]*)"$/) do |message|
+  expect(page).to have_content message
+end
+
+Then(/^current path should be the path to the movies list with the new movie "([^"]*)" added$/) do |movie|
+  expect(page.current_path).to eq(movies_path)
+  expect(page).to have_content movie
+end
+
+#Scenario 4
+
+Given(/^I have a movie titled "([^"]*)", with description "([^"]*)" and rating (\d+)$/) do |title, description, rating|
+  visit "/"
+  @movie = Movie.create(title: title, description: description, rating: rating)
+end
+
+When(/^click the link "([^"]*)"$/) do |edit|
+  click_link edit
+end
+
+When(/^I put the title "([^"]*)", description "([^"]*)" and rating (\d+)$/) do |title, description, rating|
+  fill_in "Title",with: title
+  fill_in "Description",with: description
+  fill_in "Rating",with: rating
+end
+
+Then(/^current path should be the path to the movies list with the movie edited$/) do
+  expect(page.current_path).to eq("/movies/#{@movie.id}")
+  @movie = Movie.find(@movie.id)
+  expect(page).to have_content @movie.title
+  expect(page).to have_content @movie.description
+  expect(page).to have_content @movie.rating
 end
